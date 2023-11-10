e0f007a020848d9169ef16e466f9155527d04f49
IUseAll16Letters <ch1ck3n@mail.ru>
2023-11-10 10:46:31 +0300

    stash me

---
+++ aviline/validators.py
@@ -0,0 +1,11 @@
+from django.core.exceptions import ValidationError
+
+
+def product_name_lte_64_bytes(value: str):
+    print(value)
+    value = value.encode('utf-8')
+    if len(value) > 63:
+        raise ValidationError(
+            f'''Строка "{value.decode('utf-8')}" размера больше 64 байта: {len(value)}. '''
+            'ВАЖНО! НЕ более 32-х символов кириллицей, иначе не поместиться в кнопку телеги.'
+        )
